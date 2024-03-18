5698079d2d698b1eda03f9b0704b19b78e68dca6
LB2ziK <lh.rissilb2@gmail.com>
2024-03-17 23:26:27 -0300

    Feito Desafio Código

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    int numero;
+    String agencia; 
+    String nomeCliente; 
+    double saldo;
+    Scanner sc = new Scanner(System.in);
+
+    public void entrada(){
+        System.out.println("Digite o numero: ");
+        numero = sc.nextInt();
+
+        System.out.println("Digite sua Agencia: ");
+        agencia = sc.next();
+
+        System.out.println("Digite seu nome de cliente: ");
+        nomeCliente = sc.next();
+
+        System.out.println("Digite seu Saldo: ");
+        saldo = sc.nextDouble();       
+    }
+
+    public void saida(){
+        System.out.println("Olá "+nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+    }
+    public static void main(String[] args) {    
+        ContaTerminal obj = new ContaTerminal();
+        obj.entrada();
+        obj.saida();
     }
 }
